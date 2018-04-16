96f4dcaf004a7e55b8493e0798165081e16e0adb
Gabriel Bergamim <gabriel.bergamim@outlook.com>
2018-04-15 22:44:44 -0300

    Parte 1, 2 e 3

---
+++ jogoDomino/src/Model/Jogador.java
@@ -0,0 +1,7 @@
+package Model;
+
+public class Jogador extends Usuario{
+    public Jogador(String email, String senha){
+        super(email, senha);
+    }
+}
---
+++ jogoDomino/src/Model/Usuario.java
@@ -0,0 +1,43 @@
+package Model;
+
+public abstract class Usuario {
+    private String email;
+    private String senha;
+
+    public Usuario(String email, String senha){
+        this.email = email;
+        setSenha(senha);
+    }
+
+    /**
+     * Verifica se o email é valido. É valido quando contém @ e o termino com (.com ou .com.br).
+     * @return Verdadeiro se contém e Falso se não contém.
+     */
+    public boolean validaEmail(){
+        return this.email.contains("@")&&(this.email.contains(".com")||this.email.contains(".com.br"));
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Altera o email do usuario.
+     * @param email definido para alteração.
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * Altera a senha do usuario.
+     * @param senha definida para alteração.
+     */
+    public void setSenha(String senha) {
+        this.senha = Criptografia.criptografar(senha);
+    }
+}
